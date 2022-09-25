a0782f439d782078ec6ec1730c2f334778ffc612
prechilala <puzoma16@yahoo.com>
2022-09-25 08:13:05 -0700

    fixed

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@
 def no_c(my_string):
     new_str = ""
         for i in my_string:
-            if i not in "cC":
+            if i != 'c' and i != 'C':
                  new_str += i
         return (new_str)
