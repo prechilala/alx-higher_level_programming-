9ff9979b808ec49136a9231c6be4935a4b50c70e
prechilala <puzoma16@yahoo.com>
2022-10-11 07:16:42 -0700

    hotfix

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Define a class Square."""
+
+
 class Square:
     """Represent a sqares"""
     def __init__(self, size=0, position=(0, 0)):
@@ -15,7 +17,7 @@ class Square:
     def size(self):
         """Get/set the current size of the square."""
         return (self.__size)
-    
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -23,12 +25,12 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    
+
     @property
     def position(self):
         """Get/set the current position of the square."""
         return (self.__position)
-    
+
     @position.setter
     def position(self, value):
         if (not isinstance(value, tuple) or
@@ -36,17 +38,18 @@ class Square:
                 not all(isinstance(num, int) for num in value) or
                 not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
-       self.__position = value
-    
+        self.__position = value
+
     def area(self):
         """Return the current area of the square."""
         return (self.__size * self.__size)
-    
+
     def my_print(self):
         """Print the square with the # character."""
         if self.__size == 0:
             print("")
             return
+
         [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
             [print(" ", end="") for j in range(0, self.__position[0])]
